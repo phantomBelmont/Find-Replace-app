e07bb36a3f9b8713d8408ddaf8544389a4f5fdbb
phantomBelmont <297317600+phantomBelmont@users.noreply.github.com>
2026-09-23 17:06:06 +0900

    script.js の更新

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+    //サービスワーカー登録
+    if('serviceWorker' in navigator){
+      window.addEventListener('load',()=>{
+        navigator.serviceWorker.register('./sw.js').then(reg=>console.log('SW登録成功!',reg)
+        )//thenここまで
+        .catch(err=>console.log('SW登録失敗🥲',err)
+        );//catchここまで
+      }//ロードイベントのアロー関数ここまで
+      );//イベリス ここまで
+    }//ifここまで
+
+
 // 初期化：イベントリスナーの登録
 document.addEventListener('DOMContentLoaded', () => {
     const searchBtn = document.getElementById('btn-search');
